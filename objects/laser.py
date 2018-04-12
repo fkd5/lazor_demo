3c5f47ab7666370c5d0a14c3c436b9a6f2d136b6
Fiona <36171694+fkd5@users.noreply.github.com>
2018-04-12 16:54:48 -0400

    coded update function
    has not been tested but I feel pretty good about this code because it isn't very complicated

--- objects/laser.py
+++ objects/laser.py
@@ -11,8 +11,8 @@ class Laser:
 
         DONT FORGET TO COMMENT!
         '''
-        starting_position = position
-        direction = direction
+        self.starting_position = position
+        self.direction = direction
 
     # MORE
     # Difficulty 4
@@ -36,27 +36,77 @@ class Laser:
         # **Returns**
 
         #     lasers: *list, Laser*
-        #         A list of laser objects.
+        #         A list of laser objects.  May be empty, contain 1 laser, or contain 2 lasers.
         # '''
 
-        # new position = old position + direction
+        new_position = tuple(p + d for p, d in zip(self.starting_position, self.direction))
 
         # if new position is at a point, tell point it has been hit
+        for point in points:
+            if new_position == point.position:
+                point.intersected = True
+
 
         # if new position is on edge of board, return []
+        width = len(empty_board[0])
+        height = len(empty_board)
+        if new_position[0] == 0 or new_position[0] == (width - 1) or new_position[1] == 0 or new_position[1] == (height - 1):
+            return []
 
         # check to see if the new position is hitting a block
-        # either write if statements to check if block is located at closest (even, even) point in
+        # either write if statements to check if block is located at closest (odd, odd) point in
         # direction of laser or pass to block object to check if block is hit and what side it hits
+        hit_block = None
+        shift = (0, 0)
+
+        # if x is odd
+        if starting_position[0] % 2 == 1:
+            if self.direction == (1, 1) or self.direction == (1, -1):
+                shift = (1, 0)
+            if self.direction == (-1, 1) or self.direction == (-1, -1):
+                shift = (-1, 0)
+
+        # if y is odd
+        if starting_position[1] % 2 == 1:
+            if self.direction == (1, 1) or self.direction == (-1, 1):
+                shift = (0, 1)
+            if self.direction == (1, -1) or self.direction == (-1, -1):
+                shift = (0, -1)
+
+        if shift == (0, 0):
+            print('invalid laser position or direction')
+
+        test_position = tuple(p + d for p, d in zip(new_position, shift))
+
+        hit_block = board[test_position[0]][test_position[1]]
+
+        # if laser does not hit a block, continue on
+        if hit_block == 'o' or hit_block == 'x' or hit_block == None:
+            return [laser(new_position, direction)]
 
         # if laser hits opaque block, return []
+        if hit_block.type == 'B':
+            return []
 
         # if laser hits reflect block, new direction is dependent on what side of block is hit
-        # if side parallel to y axis, new direction is (-x, y)
-        # if side parallel to x axis, new direction is (x, -y)
-        # return [laser(new position, new direction)]
+        if hit_block.type == 'A':
+            # if side parallel to y axis, new direction is (-x, y)
+            if starting_position[0] % 2 == 1:
+                new_direction = (-direction[0], direction[1])
+            # if side parallel to x axis, new direction is (x, -y)
+            if starting_position[1] % 2 == 1:
+                new_direction = (direction[0], -direction[1])
+            return [laser(new_position, new_direction)]
+
 
         # if laser hits refract block, there are two new directions
-        # one is the same direction as the reflect block
+        if hit_block.type == 'C':
+            # one is the same direction as the reflect block
+            if starting_position[0] % 2 == 1:
+                new_direction = (-direction[0], direction[1])
+            if starting_position[1] % 2 == 1:
+                new_direction = (direction[0], -direction[1])
         # one is the same as the old direction
-        # return [laser(new position, new direction), laser(new position, old direction)]
+            return [laser(new_position, new_direction), laser(new_position, direction)]
+
+        print('invalid entry in board')
